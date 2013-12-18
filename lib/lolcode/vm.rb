bb1801ac4a2610eea9532baf01a0d5b2e3bb3d3e
Ranmocy Sheng <ranmocy@gmail.com>
2013-12-18 14:25:07 -0800

    add support of HAI and KTHXBYE

--- lib/lolcode/vm.rb
+++ lib/lolcode/vm.rb
@@ -1,13 +1,37 @@
 module Lolcode
   class VM
 
+    attr_accessor :vars, :started, :verbose
+
     def initialize(options={})
+      halt
       self.verbose = options[:verbose]
-      puts 'TODO - should init a VM'
+      self.vars = {}
     end
 
     def run(line)
+      line.gsub!(/\bBTW\b.*/, '') # remove BTW comments
+
+      (start; return) if line =~ /HAI\b.*/    # start VM
+      (halt; return) if line =~ /KTHXBYE\b.*/ # halt VM
+      return unless started?
+
       puts "TODO - should run line #{line}"
+    end
+
+
+    private
+
+    def start
+      self.started = true
+    end
+
+    def halt
+      self.started = false
+    end
+
+    def started?
+      self.started
     end
 
   end
